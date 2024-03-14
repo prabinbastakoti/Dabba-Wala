7d204acf1b2b8db86f144b210ca22b731c4c6221
Prabin Bastakoti <Prabinbastakoti1@gmail.com>
2024-03-14 16:14:42 +0545

    featured

---
+++ client/components/Featured/Featured.jsx
@@ -0,0 +1,70 @@
+import './Featured.css';
+
+const restaurants = [
+  'Old Tandoori',
+  'Fine Diners',
+  'Moda',
+  'Citylights Bistro',
+  'Antique Foods',
+];
+
+const categories = [
+  'Fast Food',
+  'Casual',
+  'Fast Casual',
+  'Contemporary Casual',
+  'Fine Dining',
+];
+
+const dishes = [
+  'Burger',
+  'Butter Chicken',
+  'Katti Roll',
+  'Biryani',
+  'Ice Cream',
+];
+
+const Featured = () => {
+  return (
+    <div className="featured">
+      <div className="first">
+        <h1 className="heading">Featured Restaurants</h1>
+        <div className="tagContainer">
+          {restaurants.map((restaurant) => {
+            return (
+              <div key={restaurant} className="tags">
+                {restaurant}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="second">
+        <h1 className="heading">Explore Categories</h1>
+        <div className="tagContainer">
+          {categories.map((category) => {
+            return (
+              <div key={category} className="tags">
+                {category}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="third">
+        <h1 className="heading">Popular Dishes</h1>
+        <div className="tagContainer">
+          {dishes.map((dish) => {
+            return (
+              <div key={dish} className="tags">
+                {dish}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
--- client/routes/Homepage/Homepage.jsx
+++ client/routes/Homepage/Homepage.jsx
@@ -1,10 +1,12 @@
 import './Homepage.css';
 import Searchbar from '../../components/Searchbar/Searchbar';
+import Featured from '../../components/Featured/Featured';
 
 const Homepage = () => {
   return (
     <div className="homepage">
       <Searchbar />
+      <Featured />
     </div>
   );
 };
