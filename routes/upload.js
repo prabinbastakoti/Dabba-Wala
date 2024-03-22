33fb2cc41f749f7047f5c069f2a693ca0cd902db
Prabin Bastakoti <Prabinbastakoti1@gmail.com>
2024-03-22 16:27:02 +0545

    homepage

--- routes/upload.js
+++
@@ -1,8 +0,0 @@
-const { uploadPhoto } = require('../controllers/upload');
-const middleware = require('../utils/middleware');
-
-const router = require('express').Router();
-
-router.post('/', middleware.photosMiddleware.single('file'), uploadPhoto);
-
-module.exports = router;
